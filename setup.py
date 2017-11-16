a9d2ce79d2caeddd220500fb3d031677e8af4b01
Sergey Salnikov <serg@salnikov.ru>
2017-11-17 00:19:03 +0300

    Bump version to hide broken PyPI upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gsheetlog',
-    version='0.3.0',
+    version='0.3.1',
     description='Extract Google Sheet history',
     author='Sergey Salnikov',
     author_email='serg@salnikov.ru',
