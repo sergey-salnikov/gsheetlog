965bdb3094db2936b655b84373b16cc212ec252e
Sergey Salnikov <serg@salnikov.ru>
2018-02-01 02:16:36 +0300

    Version 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gsheetlog',
-    version='0.3.1',
+    version='0.4.0',
     description='Extract Google Sheet history',
     author='Sergey Salnikov',
     author_email='serg@salnikov.ru',
